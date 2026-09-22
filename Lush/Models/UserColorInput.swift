45d828baa618fc188e5d5cc9990ae12c7e4db74e
Tais Akemi <taakemikawaguti@gmail.com>
2026-09-22 17:24:53 -0300

    user color input

---
+++ Lush/Models/UserColorInput.swift
@@ -0,0 +1,14 @@
+//
+//  UserColorInput.swift
+//  Lush
+//
+//  Created by Tais Akemi Kawaguti on 22/09/26.
+//
+
+import Foundation
+
+struct UserColorInput {
+    let skinHex: String   // Ex: "#F5D0B1"
+    let eyeHex: String    // Ex: "#4A3B32"
+    let hairHex: String   // Ex: "#2C221E"
+}
